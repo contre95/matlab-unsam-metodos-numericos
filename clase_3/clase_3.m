c17b47b4d6be0ed066eb32bddfa9b23efe4152ad
contre95 <lucascontre95@gmail.com>
2018-08-31 16:58:30 -0300

    Dudoso Commit

--- clase_3/clase_3.m
+++ clase_3/clase_3.m
@@ -1,8 +1,24 @@
 % Buscamos la solucion de una sucecion
 % Xk+1 = -Xk +Xk
 %l = -l^3 + l
+
 x = 1
 y = -(x)^3+x
 
 while x~=y
-  x=y
+  x=y;
+  y = -(x)^3+x
+end
+
+% OTRO EJERCICIO
+
+x = 1
+y = -(x)^3+x
+k = 0
+N = 10000
+
+while x~=y && k<=N
+  x=y;
+  y = -(x)^3+x
+  k = k+1
+end
